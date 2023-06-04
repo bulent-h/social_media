f79eb316ace6dea7f9bbd8e5f091cf67d60a3a15
bulent-h <a.bulent.h@gmail.com>
2023-06-05 01:07:10 +0300

    fix

--- resources/js/Pages/Chat/MessageContainer.jsx
+++ resources/js/Pages/Chat/MessageContainer.jsx
@@ -1,13 +1,31 @@
 import MessageItem from "@/Pages/Chat/MessageItem";
 import MessageInput from '@/Pages/Chat/MessageInput';
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useState, useRef } from 'react'
 import { ChatContext } from '@/Pages/Chat/ChatContext';
+import TextInput from "@/Components/TextInput";
 export default function MessageContainer({ messages }) {
 
 
     const { auth, currentUserChat, fetchMessages } = useContext(ChatContext);
 
     const [messageItems, setMessageItems] = useState([]);
+    const [searchText, setSearchText] = useState('');
+    const [matchMessages, setMatchMessages] = useState([]);
+    const [open, setOpen] = useState(false);
+    const openRef = useRef(open);
+
+    const closeOnEscape = (e) => {
+        if (openRef.current && e.key === 'Escape') {
+            setOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', closeOnEscape);
+        return () => {
+            document.removeEventListener('keydown', closeOnEscape);
+        };
+    }, []);
 
     function mapAll(data) {
         return (
@@ -40,7 +58,6 @@ export default function MessageContainer({ messages }) {
     return (
         // <!--chat Header -->
         <>
-
             <div className="py-2 px-3 flex  flex-row justify-between items-center bg-indigo-500 dark:bg-gray-900" >
                 <div className="flex items-center">
                     <div>
@@ -56,7 +73,6 @@ export default function MessageContainer({ messages }) {
                                 </div>
                                 :
                                 <div className="bg-gray-200 dark:bg-gray-700 h-12 w-12 rounded-full"></div>
-
                         }
 
                     </div>
@@ -73,17 +89,71 @@ export default function MessageContainer({ messages }) {
                     {/* <!-- Dropdown --> */}
                     <div className="ml-6  ">
                         <div className="relative">
-                            <div >
-                                <span className="inline-flex rounded-md">
-                                    <button type="button"
-                                        className="inline-flex items-center  text-gray-900 dark:text-gray-400 ">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                                            <path fill="currentColor"
-                                                d="M15.9 14.3H15l-.3-.3c1-1.1 1.6-2.7 1.6-4.3 0-3.7-3-6.7-6.7-6.7S3 6 3 9.7s3 6.7 6.7 6.7c1.6 0 3.2-.6 4.3-1.6l.3.3v.8l5.1 5.1 1.5-1.5-5-5.2zm-6.2 0c-2.6 0-4.6-2.1-4.6-4.6s2.1-4.6 4.6-4.6 4.6 2.1 4.6 4.6-2 4.6-4.6 4.6z">
-                                            </path>
-                                        </svg>
-                                    </button>
-                                </span>
+                            <div>
+                                <div className="ml-6">
+                                    <div className="relative">
+                                        <div onClick={toggleOpen}>
+                                            <span className="inline-flex rounded-md">
+                                                <button
+                                                    type="button"
+                                                    className="inline-flex items-center border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                                                >
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                                        <path
+                                                            fill="#263238"
+                                                            fillOpacity=".5"
+                                                            d="M15.9 14.3H15l-.3-.3c1-1.1 1.6-2.7 1.6-4.3 0-3.7-3-6.7-6.7-6.7S3 6 3 9.7s3 6.7 6.7 6.7c1.6 0 3.2-.6 4.3-1.6l.3.3v.8l5.1 5.1 1.5-1.5-5-5.2zm-6.2 0c-2.6 0-4.6-2.1-4.6-4.6s2.1-4.6 4.6-4.6 4.6 2.1 4.6 4.6-2 4.6-4.6 4.6z"
+                                                        ></path>
+                                                    </svg>
+                                                </button>
+                                            </span>
+                                        </div>
+                                        {/* Full Screen Dropdown Overlay */}
+                                        {open && (
+                                            <div className="fixed inset-0 z-40 bg-black opacity-50" onClick={toggleOpen}></div>
+                                        )}
+
+                                        <div
+                                            className={`absolute z-50 mt-2 rounded-md shadow-lg origin-top-right right-0 ${open ? 'transform opacity-100 scale-100' : 'transform opacity-0 scale-95'
+                                                }`}
+                                            style={{ display: open ? 'block' : 'none' }}
+                                            // onClick={toggleOpen}
+                                        >
+                                            <div className="rounded-md ring-1 ring-white ring-opacity-5 py-1 bg-white w-96">
+                                                <div className="py-4 px-6 bg-white">
+                                                    <div className="flex flex-row items-center w-full px-4 rounded-full bg-gray-100 border-none">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                                            <path
+                                                                fill="#263238"
+                                                                fillOpacity=".5"
+                                                                d="M15.9 14.3H15l-.3-.3c1-1.1 1.6-2.7 1.6-4.3 0-3.7-3-6.7-6.7-6.7S3 6 3 9.7s3 6.7 6.7 6.7c1.6 0 3.2-.6 4.3-1.6l.3.3v.8l5.1 5.1 1.5-1.5-5-5.2zm-6.2 0c-2.6 0-4.6-2.1-4.6-4.6s2.1-4.6 4.6-4.6 4.6 2.1 4.6 4.6-2 4.6-4.6 4.6z"
+                                                            ></path>
+                                                        </svg>
+                                                        <input
+                                                            type="text"
+                                                            value={searchText}
+                                                            onChange={(e) => setSearchText(e.target.value)}
+                                                            onKeyUp={(e) => e.key === 'Enter' && search()}
+                                                            className="w-fit border-gray-100 focus:border-gray-100 focus:ring-gray-100 bg-gray-100 text-sm"
+                                                            placeholder="Search"
+                                                        />
+                                                    </div>
+                                                </div>
+
+                                                {/* <div className="bg-gray-100 flex-1">
+                                                    {matchMessages.map((matchMessage, index) => (
+                                                        <SearchMessageItem
+                                                            key={index}
+                                                            matchMessage={matchMessage}
+                                                            me={me}
+                                                            currentReceiver={currentReceiver}
+                                                        />
+                                                    ))}
+                                                </div> */}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
