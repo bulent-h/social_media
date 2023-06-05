6ec6151da551ecea1464e6b3315cdc255829b3f8
Rateb <rateb.hamsho.2002@gmail.com>
2023-06-05 17:36:53 +0300

    blocking feature's backend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Block;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -41,22 +42,45 @@ class UserController extends Controller
       })
       ->first() !== null;
 
+    // Check if the user has blocked the auth user
+    $userHasBlockedAuthUser = Block::where('blocker_id', $user->id)
+      ->where('blocked_id', $authUser->id)
+      ->exists();
+
+    if ($userHasBlockedAuthUser) {
+      return Inertia::render('RestrictedAccess');
+    }
+
+    // Check if auth user has blocked the user
+    $authUserHasBlocked = Block::where('blocker_id', $authUser->id)
+      ->where('blocked_id', $user->id)
+      ->exists();
+
     return Inertia::render('UserShow', [
       'user' => $user,
       'auth' => $authUser,
       'authUserSentFriendRequest' => $authUserSentFriendRequest,
       'authUserReceivedFriendRequest' => $authUserReceivedFriendRequest,
       'isFriends' => $isFriends,
+      'authUserHasBlocked' => $authUserHasBlocked,
       'csrf' => csrf_token(),
     ]);
   }
+
 
   public function findFriends(Request $request)
   {
     $keyword = $request->input('keyword');
     $loggedInUserId = auth()->user()->id;
 
+    $blockedUsersIds = Block::where('blocker_id', $loggedInUserId)
+      ->orWhere('blocked_id', $loggedInUserId)
+      ->pluck('blocked_id', 'blocker_id')
+      ->toArray();
+
     $users = User::where('id', '!=', $loggedInUserId)
+      ->whereNotIn('id', array_keys($blockedUsersIds))
+      ->whereNotIn('id', array_values($blockedUsersIds))
       ->where(function ($query) use ($keyword) {
         $query->where('name', 'like', "%{$keyword}%")
           ->orWhere('username', 'like', "%{$keyword}%");
@@ -68,4 +92,5 @@ class UserController extends Controller
       'keyword' => $keyword,
     ]);
   }
+
 }
