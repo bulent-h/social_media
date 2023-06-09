38d77d9e5e385f39043440c2f24e1e04920604ef
Rateb <rateb.hamsho.2002@gmail.com>
2023-06-09 08:02:29 +0300

    create post frontend

--- resources/js/Pages/CreatePost.jsx
+++ resources/js/Pages/CreatePost.jsx
@@ -57,7 +57,7 @@ export default function CreatePost({ auth }) {
                         <select
                             value={type}
                             onChange={e => setType(e.target.value)}
-                            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-md"
+                            className="mt-1 block w-full pl-3 pr-10 py-2 text-base bg-gray-200 border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-full"
                         >
                             <option value="">-- Select post type --</option>
                             <option value="text">Text</option>
@@ -75,8 +75,7 @@ export default function CreatePost({ auth }) {
                             <textarea
                                 value={content}
                                 onChange={e => setContent(e.target.value)}
-                                placeholder="Write your content"
-                                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-md"
+                                className="mt-1 block w-full pl-3 pr-10 py-4 text-base bg-gray-200 border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-xl"
                             />
                         </div>
                     )}
@@ -90,7 +89,7 @@ export default function CreatePost({ auth }) {
                                 type="file"
                                 accept={`${type}/*`}
                                 onChange={handleFileChange}
-                                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-md"
+                                className="mt-1 block w-full pl-3 pr-10 py-2 text-base   border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-full"
                             />
                         </div>
                     )}
@@ -106,7 +105,7 @@ export default function CreatePost({ auth }) {
                                 value={pollQuestion}
                                 onChange={e => setPollQuestion(e.target.value)}
                                 placeholder="Question"
-                                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-md"
+                                className="mt-1 mb-4 block w-full pl-3 pr-10 py-2 text-base bg-gray-200 border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-lg"
                             />
                             {pollOptions.map((option, index) => (
                                 <input
@@ -119,18 +118,18 @@ export default function CreatePost({ auth }) {
                                         setPollOptions(newOptions);
                                     }}
                                     placeholder={`Option ${index + 1}`}
-                                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-md"
+                                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base bg-gray-200 border-gray-300 focus:outline-none focus:ring-purple-700 focus:border-purple-800 sm:text-sm rounded-lg"
                                 />
                             ))}
                             <div className="flex space-x-2 mt-4">
-                                <button type="button" onClick={addOption} className="px-3 py-2 rounded-md text-sm font-medium text-white bg-purple-700 hover:bg-purple-800 focus:outline-none">Add option</button>
-                                <button type="button" onClick={removeOption} className="px-3 py-2 rounded-md text-sm font-medium text-white bg-purple-700 hover:bg-purple-800 focus:outline-none">Remove option</button>
+                                <button type="button" onClick={addOption} className="px-3 py-2 rounded-md text-sm font-medium text-purple-700 hover:hover:scale-105 focus:outline-none">Add option</button>
+                                <button type="button" onClick={removeOption} className="px-3 py-2 rounded-md text-sm font-medium text-purple-700 hover:hover:scale-105 focus:outline-none">Remove option</button>
                             </div>
                         </div>
                     )}
 
-                    <div>
-                        <button type="submit" className="w-full px-3 py-2 rounded-md text-sm font-medium text-white bg-purple-700 hover:bg-indigo-800 focus:outline-none">Create post</button>
+                    <div className='flex justify-end'>
+                        <button type="submit" className="px-5 py-2 rounded-full text-sm font-medium text-white bg-purple-700 hover:bg-indigo-800 focus:outline-none">Create post</button>
                     </div>
                 </form>
             </div>
